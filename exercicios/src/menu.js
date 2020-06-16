0358a045d8d7b104809018d410107c9f00c796fb
juniorlourenco <juniorminaj101@gmail.com>
2020-06-16 13:22:04 -0400

    adding Platform

--- exercicios/src/menu.js
+++ exercicios/src/menu.js
@@ -4,8 +4,12 @@ import { CreateDrawerNavigator } from 'react-navigation';
 import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar';
 import Inverter, { MegaSena } from './componentes/Multi';
+import Contador from './componentes/contador';
 
 export default CreateDrawerNavigator({
+    Contador: {
+        screen: () => <Contador />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={8} />,
         navigationOptions: {title: 'Mega Sena'}
