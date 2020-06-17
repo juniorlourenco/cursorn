25cf9231ede4cab8a6a36d452dc1177bdadc1ef1
juniorlourenco <juniorminaj101@gmail.com>
2020-06-17 14:23:53 -0400

    1st commit calculator

---
+++ calculator/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome</Text>
+      </View>
+    );
+  }
+}
